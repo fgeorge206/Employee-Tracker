dc44578aa3486c3fc892b5bebfc5e75a259fa4be
Febin George <you@example.com>
2023-03-08 18:03:40 -0800

    updates

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function promptUser(){
 			type: "list",
 			name: "option",
 			message: "What do you want to do?",
-			choices: ["view all departments", "view all roles", "view all employees"]
+			choices: ["view all departments", "view all roles", "view all employees", "add department", "add role", "add employee", "quit"]
 		}
 	]).then(function(results){
 		console.log(results)
@@ -36,6 +36,36 @@ function promptUser(){
 			})
 		}
 		if (results.option === "view all employees") {
+			db.query("SELECT * FROM employee",function(err,data){
+				if(err) throw err
+				console.table(data)
+				promptUser();
+			})
+		}
+		if (results.option === "add department") {
+			inquirer.prompt({
+				type: "input",
+				message: "Please enter the name of the department.",
+				name: "department",
+			}).then(function(results){
+				db.promise()
+					.query(`INSERT INTO department (name) VALUES (?)`, results.department)
+					.then(()=>{
+						console.log(`${results.department} has been added to the database`);
+					})
+					.catch((err)=>{
+						throw err;
+					})
+			})
+		}
+		if (results.option === "add role") {
+			db.query("SELECT * FROM employee",function(err,data){
+				if(err) throw err
+				console.table(data)
+				promptUser();
+			})
+		}
+		if (results.option === "add employee") {
 			db.query("SELECT * FROM employee",function(err,data){
 				if(err) throw err
 				console.table(data)
