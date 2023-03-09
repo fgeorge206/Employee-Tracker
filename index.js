c6834ab0e634f0d55c4a99682acf5c5d69c1c059
Febin George <you@example.com>
2023-03-08 16:08:47 -0800

    updates

--- index.js
+++ index.js
@@ -21,6 +21,27 @@ function promptUser(){
 		}
 	]).then(function(results){
 		console.log(results)
+		if (results.option === "view all departments") {
+			db.query("SELECT * FROM department",function(err,data){
+				if(err) throw err
+				console.table(data)
+				promptUser();
+			})
+		}
+		if (results.option === "view all roles") {
+			db.query("SELECT * FROM role",function(err,data){
+				if(err) throw err
+				console.table(data)
+				promptUser();
+			})
+		}
+		if (results.option === "view all employees") {
+			db.query("SELECT * FROM employee",function(err,data){
+				if(err) throw err
+				console.table(data)
+				promptUser();
+			})
+		}
 	})
 }
 db.connect(function(err){
